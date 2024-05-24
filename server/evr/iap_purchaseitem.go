9d489525fc044751e9cffce343642f80e1ada9f0
Andrew Bates <a@sprock.io>
2024-05-24 17:01:21 -0500

    Add purchaseItem stub

---
+++ server/evr/iap_purchaseitem.go
@@ -0,0 +1,31 @@
+package evr
+
+import (
+	"fmt"
+)
+
+// SNSPurchaseItems
+type IAPPurchaseItems struct {
+}
+
+func (m IAPPurchaseItems) Token() string {
+	return "SNSIAPPurchaseItem"
+}
+
+func (m IAPPurchaseItems) Symbol() Symbol {
+	return ToSymbol(m.Token())
+}
+
+func (m IAPPurchaseItems) String() string {
+	return fmt.Sprintf("%s()", m.Token())
+}
+
+func (r *IAPPurchaseItems) Stream(s *EasyStream) error {
+	return RunErrorFunctions([]func() error{
+		func() error { return s.Skip(1) },
+	})
+}
+
+func (r *IAPPurchaseItems) ToString() string {
+	return fmt.Sprintf("%s()", r.Token())
+}
